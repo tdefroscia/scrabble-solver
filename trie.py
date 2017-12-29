c548fc2b3a1bd98f2507a2c1d89aab95a6853e10
Sathya Anisetti <sathya_anisetti@brown.edu>
2017-12-29 13:22:51 -0500

    Update trie.py

--- trie.py
+++ trie.py
@@ -1,18 +1,8 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
 class trieNode:
     def __init__(self, letter, termNode=0, children=[]):
         self.letter = letter
         self.termNode = termNode
         self.children = children
-
-
-# In[2]:
-
 
 def trieConstructor(node, i, line):
     if (i > len(line)-1):
@@ -24,11 +14,8 @@ def trieConstructor(node, i, line):
     node.children.insert(0, trieNode(line[i], termNode=0, children=[]))
     return trieConstructor(node.children[0], i+1, line)
 
-
-# In[3]:
-
-
 root = trieNode(None)
+
 file = open("dictionary.txt", "r")
 for line in file:
     trieConstructor(root, 0, line)
