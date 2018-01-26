e62788fe70f4fad7df6779a8fa498396e278bd7b
Sathya <sathya_anisetti@brown.edu>
2018-01-25 20:18:06 -0500

    initial unittests for gaddag

--- scrabble_solving_engine/tests/GADDAG_Tests/test_gaddag.py
+++ scrabble_solving_engine/tests/GADDAG_Tests/test_gaddag.py
@@ -1,18 +1,45 @@
+import unittest
 from scrabble_solving_engine.GADDAG import GADDAG
 
-def test_one():
-    gaddag = GADDAG('one_word_test.txt')
-    print(gaddag.initialState.letter_set)
-    print(gaddag.initialState.arcs[0].letter + ' ' + gaddag.initialState.arcs[0].next_state.arcs[0].letter + ' ' + gaddag.initialState.arcs[0].next_state.arcs[0].next_state.arcs[0].letter + ' ' + str(gaddag.initialState.arcs[0].next_state.arcs[0].next_state.arcs[0].next_state.letter_set))
-    print(gaddag.initialState.arcs[1].letter + ' ' + gaddag.initialState.arcs[1].next_state.arcs[0].letter + ' ' + gaddag.initialState.arcs[1].next_state.arcs[0].next_state.arcs[0].letter + ' ' + gaddag.initialState.arcs[1].next_state.arcs[0].next_state.arcs[0].next_state.arcs[0].letter + ' ' + str(gaddag.initialState.arcs[1].next_state.arcs[0].next_state.arcs[0].next_state.arcs[0].next_state.letter_set))
-    print(gaddag.initialState.arcs[2].letter + ' ' + gaddag.initialState.arcs[2].next_state.arcs[0].letter + ' ' + gaddag.initialState.arcs[2].next_state.arcs[0].next_state.arcs[0].letter + ' ' + gaddag.initialState.arcs[2].next_state.arcs[0].next_state.arcs[0].next_state.arcs[0].letter + ' ' + str(gaddag.initialState.arcs[2].next_state.arcs[0].next_state.arcs[0].next_state.arcs[0].next_state.letter_set))
-    print(gaddag.initialState.arcs[3].letter + ' ' + gaddag.initialState.arcs[3].next_state.arcs[0].letter + ' ' + gaddag.initialState.arcs[3].next_state.arcs[0].next_state.arcs[0].letter + ' ' + gaddag.initialState.arcs[3].next_state.arcs[0].next_state.arcs[0].next_state.arcs[0].letter + ' ' + str(gaddag.initialState.arcs[3].next_state.arcs[0].next_state.arcs[0].next_state.arcs[0].next_state.letter_set))
+class TestGaddag(unittest.TestCase):
+    def test_one_word(self):
+        gaddag = GADDAG('one_word_test.txt')
+        self.assertEqual(gaddag.initialState.letter_set, {'c', 'a', 'r', 'e'})
+        self.assertEqual(gaddag.initialState.arcs[0].letter \
+                         + ' ' + gaddag.initialState.arcs[0].next_state.arcs[0].letter \
+                         + ' ' + gaddag.initialState.arcs[0].next_state.arcs[0].next_state.arcs[0].letter \
+                         + ' ' + gaddag.initialState.arcs[0].next_state.arcs[0].next_state.arcs[0]\
+                         .next_state.arcs[0].letter,
+                         'e r a c')
+        self.assertEqual(gaddag.initialState.arcs[1].letter
+              + ' ' + gaddag.initialState.arcs[1].next_state.arcs[0].letter
+              + ' ' + gaddag.initialState.arcs[1].next_state.arcs[0].next_state.arcs[0].letter
+              + ' ' + gaddag.initialState.arcs[1].next_state.arcs[0].next_state.arcs[0].next_state.arcs[0].letter
+              + ' ' + gaddag.initialState.arcs[1].next_state.arcs[0].next_state.arcs[0].next_state.arcs[0]\
+              .next_state.arcs[0].letter,
+              'r a c | e')
+        self.assertEqual(gaddag.initialState.arcs[2].letter
+              + ' ' + gaddag.initialState.arcs[2].next_state.arcs[0].letter
+              + ' ' + gaddag.initialState.arcs[2].next_state.arcs[0].next_state.arcs[0].letter
+              + ' ' + gaddag.initialState.arcs[2].next_state.arcs[0].next_state.arcs[0].next_state.arcs[0].letter
+              + ' ' + gaddag.initialState.arcs[2].next_state.arcs[0].next_state.arcs[0].next_state.arcs[0]\
+              .next_state.arcs[0].letter,
+              'a c | r e')
+        self.assertEqual(gaddag.initialState.arcs[3].letter
+                        + ' ' + gaddag.initialState.arcs[3].next_state.arcs[0].letter
+              + ' ' + gaddag.initialState.arcs[3].next_state.arcs[0].next_state.arcs[0].letter
+              + ' ' + gaddag.initialState.arcs[3].next_state.arcs[0].next_state.arcs[0].next_state.arcs[0].letter
+              + ' ' + gaddag.initialState.arcs[3].next_state.arcs[0].next_state.arcs[0].next_state.arcs[0]\
+              .next_state.arcs[0].letter,
+              'c | a r e')
+
 
 def dictionary():
     print("started")
     GADDAG('dictionary.txt')
     print("done")
 
+
 if __name__ == '__main__':
-    test_one()
+    unittest.main()
     dictionary()
